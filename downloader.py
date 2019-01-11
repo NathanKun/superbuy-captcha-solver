8078e38a3d35497bae00a15252c68ac279146186
HE Junyang <nathanhejunyang@gmail.com>
2019-01-11 17:37:11 +0100

    Download captcha first and rename them manually after

--- downloader.py
+++ downloader.py
@@ -2,6 +2,7 @@ import os
 import random
 import string
 import threading
+import time
 from datetime import datetime
 from io import BytesIO
 
@@ -16,14 +17,14 @@ class downloadCaptchaThread (threading.Thread):
     def __init__(self, threadID, headers, captchaText, imgsPerThread, targetFolder):
         threading.Thread.__init__(self)
         self.threadID = threadID
-        self.name = 'Thread-' + str(threadID) + '-' + captchaText
+        self.name = 'T-' + str(threadID) + '-' + captchaText
         self.headers = headers
         self.captchaText = captchaText
         self.imgsPerThread = imgsPerThread
         self.targetFolder = targetFolder
         
     def run(self):
-        print ("Start Thread：" + self.name)
+        print ("Start Thread " + self.name)
 
         # download images
         print('Downloading ' + str(self.imgsPerThread) + ' images for captcha ' + self.captchaText + '...')
@@ -31,7 +32,7 @@ class downloadCaptchaThread (threading.Thread):
             with open(self.targetFolder + str(i) + '.png', 'wb') as f:
                 f.write(requests.get(url, headers=self.headers).content)
         
-        print ("End Thread：" + self.name)
+        print ("End Thread " + self.name)
 
 
 
@@ -43,7 +44,7 @@ def genCookieHeaders():
 
 def main():
     imgsPerThread = 128
-    nbOfThreads = 16
+    nbOfThreads = 64
     threads = [None] * nbOfThreads
 
     for threadCount in range(nbOfThreads):
@@ -54,17 +55,19 @@ def main():
         requestContent = requests.get(url, headers = headers).content
 
         # convert to image
-        firstImage = Image.open(BytesIO(requestContent))
+        #firstImage = Image.open(BytesIO(requestContent))
 
         # plot
-        root = tk.Tk()
-        tkimage = ImageTk.PhotoImage(firstImage)
-        tk.Label(root, image=tkimage).pack()
-        root.focus_force()
-        root.mainloop()
+        #root = tk.Tk()
+        #tkimage = ImageTk.PhotoImage(firstImage)
+        #tk.Label(root, image=tkimage).pack()
+        #root.focus_force()
+        #root.mainloop()
 
         # input the captcha text
-        captchaText = input('Enter the captcha:\n')
+        #captchaText = input('Enter the captcha:\n')
+        
+        captchaText = ''.join(random.choices(string.ascii_uppercase, k=10))
         
         # mkdir
         targetFolder = folder + captchaText + '\\'
@@ -77,6 +80,8 @@ def main():
         thread = downloadCaptchaThread(threadCount, headers, captchaText, imgsPerThread, targetFolder)
         thread.start()
         threads[threadCount] = thread
+        
+        time.sleep(1)
 
     for t in threads:
         t.join()
